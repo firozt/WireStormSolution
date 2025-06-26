7d4015de4fed9929c8bf193290a96c678a968c1d
firozt <firozt03@gmail.com>
2025-06-26 22:45:49 +0100

    starting challenge

--- main.cpp
+++ main.cpp
@@ -2,13 +2,10 @@
 #include <sys/types.h> 
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <stdio.h>
-#include <unistd.h> 
-#include <string.h> 
+#include <unistd.h>
 #include <thread>
 #include <set>
 #include <mutex>
-#include <atomic>
 /*
     REQUIREMENTS: 
 
@@ -38,6 +35,7 @@
 
 #define SOURCE_PORT 33333
 #define DESTINATION_PORT 44444
+#define MAX_BUFFER_SIZE 1024
 
 
 
@@ -48,13 +46,9 @@ private:
     std::set<int> clients;
     int max_connections;
 public:
+    ConnectionManager(int max_connections) : max_connections(max_connections) {}
 
-    ConnectionManager(int max_connections) {
-        this->max_connections = max_connections;
-    };
-
-
-    int get_max_connections() {
+    int get_max_connections() const {
         return max_connections;
     }
 
@@ -85,10 +79,57 @@ public:
     }
 };
 
-// TODO
+
+// simple struct for representing the CTMP message
+struct CTMPMessage {
+    uint16_t payload_length;
+    uint8_t* payload;
+};
+
 class CTMPMessageValidator {
     public:
-    static bool validate(const char* data, int len);
+    static bool validate(const uint8_t* data, size_t len) {
+        constexpr uint8_t magic_byte = 0xCC;
+        constexpr uint8_t padding_byte = 0x00;
+        constexpr uint8_t header_size = 8;
+
+        if (len < header_size) return false; // entire message cant be less than the header size
+
+        // check magic byte and padding byte
+        if (data[0] != magic_byte) return false;
+        if (data[1] != padding_byte) return false;
+
+        // check payload length
+        uint16_t payload_length = get_payload_length(data);
+        if (payload_length < 0) return false;
+
+
+        // Check 4 padding bytes (bytes 4 to 7)
+        for (int i = 4; i < 8; ++i) {
+            if (data[i] != padding_byte) return false;
+        }
+
+
+        // if the data exceeds the length specified in header, return false
+        uint16_t actual_payload_size = len - header_size;
+        if (actual_payload_size > payload_length) return false;
+
+        return true;
+    }
+
+
+    static uint16_t get_payload_length(const uint8_t* data) {
+        // payload length cannot be in this data
+        if (sizeof(data) < 4) {
+            return -1;
+        }
+        // extract payload length from ctmp message
+        // copy the next 2 bytes, convert it from network order.
+        uint16_t payload_length_network_order;
+        std::memcpy(&payload_length_network_order, &data[2], sizeof(uint16_t));
+        uint16_t payload_length = ntohs(payload_length_network_order);
+        return payload_length;
+    }
 };
 
 enum CLIENT_TYPE {
@@ -112,9 +153,9 @@ class ServerListener {
     * @return Returns the socket field descriptor created with the socket sys call if 
     * successfull, otherwise returns -1, and perrors the error.
     */
-    int create_socket(sockaddr_in address, int addr_len) {
+    int create_socket(sockaddr_in address, int addr_len) const {
         // 1) create socket
-        // socket with ipv4 protocol (af_inet), via tcp (sock_stream)
+        // ipv4 protocol (af_inet), via tcp (sock_stream)
         int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
         // socket_fd should be > 0 to be valid else error
         if (socket_fd < 0) {
@@ -129,7 +170,7 @@ class ServerListener {
             printf("Unable to bind socket for port %d\n",port);
             return -1;
         }
-        // 3) listen to binded socket at client port
+        // 3) listen to tne binded socket at client port
         // takes socket field descriptor and a max len of the queue
         int listen_status = listen(socket_fd, 10);
 
@@ -141,33 +182,63 @@ class ServerListener {
     }
 
 
-    void source_listener(int conn_fd,int MAX, ConnectionManager* destination_clients) {
-        char buff[MAX]; 
+    static void source_listener(int conn_fd, int MAX, ConnectionManager* destination_clients) {
+        std::vector<uint8_t> message;
+        message.reserve(MAX);
+        uint8_t buffer[MAX/2];
 
-        while(1) { 
-            // reset buffer
-            bzero(buff, MAX); 
-            
-            // read the message from client and copy it in buffer 
-            ssize_t msg_bytes = recv(conn_fd, buff, MAX, 0); // blocks execution
-            if (msg_bytes <= 0) {
-                if (msg_bytes == 0) {
-                    printf("Source client has disconnected gracefully\n");
+        while (true) {
+            ssize_t bytes_received = recv(conn_fd, buffer, MAX, 0);
+            if (bytes_received <= 0) {
+                if (bytes_received == 0) {
+                    printf("Source client disconnected gracefully\n");
                 } else {
-                    perror("Source client recv error\n");
+                    perror("Source client recv error");
                 }
                 break;
             }
-            for (int conn : destination_clients->get_all()) {
-                if (send(conn, buff, msg_bytes, 0) < 0) {
-                    perror("Unable to send message to destination client");
+
+            // append newly received bytes to buffer
+            message.insert(message.end(), buffer, buffer + bytes_received);
+
+            // Process all full messages in buffer
+            while (message.size() >= 8) {
+                // extract payload length from bytes 2 and 3 and convert to host order
+                int payload_length = CTMPMessageValidator::get_payload_length(message.data());
+                if (payload_length < 0) {
+                    perror("Source client payload length is invalid");
+                    break;
                 }
+
+                size_t full_msg_len = 8 + payload_length;
+
+                if (message.size() < full_msg_len) {
+                    // Not enough data yet for full message
+                    break;
+                }
+
+                // Validate the full message
+                if (!CTMPMessageValidator::validate(message.data(), full_msg_len)) {
+                    // Invalid message, drop these bytes (or handle differently)
+                    message.erase(message.begin(), message.begin() + full_msg_len);
+                    continue;
+                }
+
+                // send the valid ctmp message to all destination clients
+                for (int conn : destination_clients->get_all()) {
+                    if (send(conn, message.data(), full_msg_len, 0) < 0) {
+                        perror("Unable to send message to destination client");
+                    }
+                }
+
+                // Remove the processed message from buffer
+                message.erase(message.begin(), message.begin() + full_msg_len);
             }
-        } 
-    } 
+        }
+    }
 
     // handles the destination clients actions
-    void destination_listener(int conn_fd, int MAX) {
+    static void destination_listener(int conn_fd, int MAX) {
         char buff[MAX]; 
         while (true) {
             bzero(buff, MAX); 
@@ -204,7 +275,7 @@ class ServerListener {
 
 
         // create address object
-        struct sockaddr_in address;
+        struct sockaddr_in address{};
         address.sin_family = AF_INET;
         address.sin_addr.s_addr = INADDR_ANY;
         address.sin_port = htons(port);
@@ -223,8 +294,8 @@ class ServerListener {
         // is spawned and will handle broadcasted messages from source thread
         // number of destination clients will be handled by the destination client list
         
-        while(1) {
-            int conn_fd = accept(socket_fd, (struct sockaddr*) &address, (socklen_t*)&addr_len); 
+        while(true) {
+            int conn_fd = accept(socket_fd, (struct sockaddr*) &address, (socklen_t*)&addr_len);
             if (conn_fd < 0) { 
                 perror("Failed to accept a source client\n");
                 continue;
@@ -243,8 +314,8 @@ class ServerListener {
             connections.add(conn_fd);
             std::thread t([this,conn_fd, destination_clients]() {
                 // use corresponding handler
-                if (type == SOURCE) this->source_listener(conn_fd, 100, destination_clients);
-                if (type == DESTINATION) this->destination_listener(conn_fd, 100);
+                if (type == SOURCE) this->source_listener(conn_fd, MAX_BUFFER_SIZE, destination_clients);
+                if (type == DESTINATION) this->destination_listener(conn_fd, MAX_BUFFER_SIZE);
                 // cleanup socket from connection list
                 connections.remove(conn_fd);
                 close(conn_fd);
@@ -258,6 +329,7 @@ class ServerListener {
 
 int main() {
     // create two listening servers, one for source and destination
+    // source has max connections of 1, destination has unbound (-1)
     ServerListener source(SOURCE_PORT,SOURCE, 1);
     ServerListener destination(DESTINATION_PORT, DESTINATION, -1);
 
